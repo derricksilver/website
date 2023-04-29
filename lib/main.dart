5cbabf826cdba08a1ef9e9f8566df5de12bea0a6
Jeffery-Bernasko <57768306+Jeffery-Bernasko@users.noreply.github.com>
2023-04-29 23:39:51 +0000

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class Fool extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: LandPage()
     );
   }
